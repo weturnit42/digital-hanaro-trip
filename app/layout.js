b0eb7ce7239c2132e3482b6d5e10394e51a9c3fc
weturnit42 <we_turn_it42@naver.com>
2024-04-30 19:08:59 +0900

    add metadata in layout.js

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,11 @@ import Navbar from "@/components/Navbar";
 import "./globals.css";
 import { ChatProvider } from "./provider";
 
+export const metadata = {
+  httpEquiv: 'Content-Security-Policy',
+  content: "upgrade-insecure-requests"
+}
+
 const inter = Inter({
   subsets: ["latin"],
   display: "swap",
